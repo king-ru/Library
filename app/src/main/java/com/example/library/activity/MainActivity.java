0fcadf870fdce62d3179c70e96ee796098c487ad
king-ru <1262120843@qq.com>
2018-10-01 10:22:28 +0800

    first

---
+++ app/src/main/java/com/example/library/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.library.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.library.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
